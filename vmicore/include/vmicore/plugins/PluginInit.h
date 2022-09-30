504dc12da6c667055715110117a37a880dd0a0fe
Dorian Eikenberg <dorian.eikenberg@gdata.de>
2022-09-30 11:34:09 +0200

    Make plugin api version check more user friendly

--- vmicore/include/vmicore/plugins/PluginInit.h
+++ vmicore/include/vmicore/plugins/PluginInit.h
@@ -3,8 +3,13 @@
 
 #include "PluginInterface.h"
 
+#define VMI_PLUGIN_API_VERSION_INFO                                                                                    \
+    [[maybe_unused]] const uint8_t VmiCore::Plugin::API_VERSION = VmiCore::Plugin::PluginInterface::API_VERSION;
+
 namespace VmiCore::Plugin
 {
+    extern const uint8_t API_VERSION;
+
     extern "C" bool
     init(PluginInterface* pluginInterface, std::shared_ptr<IPluginConfig> config, std::vector<std::string> args);
 }
