7b57bcd7e9bd8ad5ef3cc73984058d767cbc7321
Dorian Eikenberg <dorian.eikenberg@gdata.de>
2022-12-01 13:32:54 +0100

    Fix IntrospectionAPI mock

--- vmicore/test/include/vmicore_test/vmi/mock_IntrospectionAPI.h
+++ vmicore/test/include/vmicore_test/vmi/mock_IntrospectionAPI.h
@@ -23,34 +23,34 @@ namespace VmiCore
 
         MOCK_METHOD(uint64_t, getCurrentVmId, (), (const override));
 
-        MOCK_METHOD(uint, getNumberOfVCPUs, (), (override));
+        MOCK_METHOD(uint, getNumberOfVCPUs, (), (const override));
 
         MOCK_METHOD(addr_t, translateKernelSymbolToVA, (const std::string&), (override));
 
         MOCK_METHOD(addr_t, translateUserlandSymbolToVA, (addr_t, addr_t, const std::string&), (override));
 
-        MOCK_METHOD(addr_t, convertVAToPA, (addr_t, addr_t), (override));
+        MOCK_METHOD(addr_t, convertVAToPA, (addr_t, addr_t), (const override));
 
-        MOCK_METHOD(addr_t, convertPidToDtb, (pid_t), (override));
+        MOCK_METHOD(addr_t, convertPidToDtb, (pid_t), (const override));
 
-        MOCK_METHOD(pid_t, convertDtbToPid, (addr_t), (override));
+        MOCK_METHOD(pid_t, convertDtbToPid, (addr_t), (const override));
 
         MOCK_METHOD(std::unique_ptr<std::string>, extractUnicodeStringAtVA, (addr_t, addr_t), (override));
 
         MOCK_METHOD(std::unique_ptr<std::string>, extractStringAtVA, (addr_t, addr_t), (override));
 
-        MOCK_METHOD(OperatingSystem, getOsType, (), (override));
+        MOCK_METHOD(OperatingSystem, getOsType, (), (const override));
 
-        MOCK_METHOD(uint64_t, getOffset, (const std::string&), (override));
+        MOCK_METHOD(uint64_t, getOffset, (const std::string&), (const override));
 
-        MOCK_METHOD(addr_t, getKernelStructOffset, (const std::string&, const std::string&), (override));
+        MOCK_METHOD(addr_t, getKernelStructOffset, (const std::string&, const std::string&), (const override));
 
-        MOCK_METHOD(bool, isInitialized, (), (override));
+        MOCK_METHOD(bool, isInitialized, (), (const override));
 
-        MOCK_METHOD((std::tuple<addr_t, size_t, size_t>),
+        MOCK_METHOD((std::tuple<addr_t, std::size_t, std::size_t>),
                     getBitfieldOffsetAndSizeFromJson,
                     (const std::string&, const std::string&),
-                    (override));
+                    (const override));
 
         MOCK_METHOD(size_t, getStructSizeFromJson, (const std::string&), (const override));
 
