60171a795d6c483ddbb1a263c3441ef07c74022f
Dorian Eikenberg <dorian.eikenberg@gdata.de>
2022-12-01 13:20:43 +0100

    Build dummy target to test potentially unused mocks

---
+++ vmicore/test/mocks/MockAggregator.cpp
@@ -0,0 +1,6 @@
+#include <vmicore_test/os/mock_MemoryRegionExtractor.h>
+#include <vmicore_test/os/mock_PageProtection.h>
+#include <vmicore_test/plugins/mock_PluginInterface.h>
+#include <vmicore_test/vmi/mock_Breakpoint.h>
+#include <vmicore_test/vmi/mock_InterruptEvent.h>
+#include <vmicore_test/vmi/mock_IntrospectionAPI.h>
