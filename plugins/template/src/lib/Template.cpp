2e2cfc62db2ca8917ee02b67f643cfc716aff95b
Dorian Eikenberg <dorian.eikenberg@gdata.de>
2023-06-06 09:56:57 +0200

    Template: Adapt for new plugin lifetime management

--- plugins/template/src/lib/Template.cpp
+++ plugins/template/src/lib/Template.cpp
@@ -6,10 +6,9 @@
 #define BUILD_VERSION "TestBuild"
 #endif
 
+#include "Template.h"
 #include "Filenames.h"
-#include "TemplateCode.h"
 #include <tclap/CmdLine.h>
-#include <vmicore/plugins/PluginInit.h>
 
 // Ensure API compatibility between plugin and VmiCore. When loading a plugin the API version against which it was
 // compiled is checked in VmiCore.
@@ -17,37 +16,18 @@ VMI_PLUGIN_API_VERSION_INFO
 
 using VmiCore::ActiveProcessInformation;
 using VmiCore::ILogger;
+using VmiCore::Plugin::IPlugin;
 using VmiCore::Plugin::IPluginConfig;
 using VmiCore::Plugin::PluginInterface;
 
-// This file contains the main interface between the VmiCore and the plugin. Here resides the init-function as well as
-// the callbacks for every event.
+// This file contains the main interface between the VmiCore and the plugin. Here resides the init function.
 namespace Template
 {
-
-    namespace
+    Template::Template(VmiCore::Plugin::PluginInterface* pluginInterface,
+                       const VmiCore::Plugin::IPluginConfig& config,
+                       std::vector<std::string>& args)
+        : logger(pluginInterface->newNamedLogger(TEMPLATE_LOGGER_NAME))
     {
-        std::unique_ptr<ILogger> logger;
-        PluginInterface* pluginInterface;
-        std::shared_ptr<TemplateCode> templateCode;
-    }
-
-    // As an example when a new process is started this callback is called. Check the PluginInterface.h for additional
-    // available events.
-    void processStartCallback(std::shared_ptr<const ActiveProcessInformation> processInformation)
-    {
-        logger->info("Process started", {{"Process", *processInformation->fullName}, {"Pid", processInformation->pid}});
-        templateCode->doStuffWithProcessStart(*processInformation);
-    }
-
-    // This is the init function. It gets called by the VmiCore and is responsible for setting up logging, command line
-    // parameters and other required functionality.
-    extern "C" bool init(PluginInterface* communicator,
-                         std::shared_ptr<IPluginConfig> config, // NOLINT(performance-unnecessary-value-param)
-                         std::vector<std::string> args)
-    {
-        pluginInterface = communicator;
-
         // Set up command line parameter with TCLAP as needed.
         TCLAP::CmdLine cmd("Plugin Template for VMICore.", ' ', PLUGIN_VERSION);
         TCLAP::ValueArg parameter1{"p",
@@ -59,7 +39,6 @@ namespace Template
                                    cmd};
         cmd.parse(args);
 
-        logger = pluginInterface->newNamedLogger(TEMPLATE_LOGGER_NAME);
         logger->debug("Template plugin version info", {{"Version", PLUGIN_VERSION}, {"BuildNumber", BUILD_VERSION}});
 
         if (parameter1.isSet())
@@ -68,21 +47,16 @@ namespace Template
         }
 
         // Create the objects which contain the logic of the plugin
-        templateCode = std::make_shared<TemplateCode>(pluginInterface, pluginInterface->getIntrospectionAPI());
-
-        // Register required events
-        try
-        {
-            pluginInterface->registerProcessStartEvent(processStartCallback);
-        }
-        catch (const std::exception& exc)
-        {
-            logger->error("Could not register ProcessStart Event", {{"Exception", exc.what()}});
-            return false;
-        }
-
-        logger->debug("Template plugin: init end");
-
-        return true;
+        templateCode = std::make_unique<TemplateCode>(pluginInterface, pluginInterface->getIntrospectionAPI());
     }
+}
+
+// This is the init function. It is called by VmiCore and is responsible for creating an instance of a plugin.
+// All plugins have to inherit from IPlugin.
+std::unique_ptr<IPlugin>
+VmiCore::Plugin::init(PluginInterface* pluginInterface,
+                      std::shared_ptr<IPluginConfig> config, // NOLINT(performance-unnecessary-value-param)
+                      std::vector<std::string> args)
+{
+    return std::make_unique<Template::Template>(pluginInterface, *config, args);
 }
